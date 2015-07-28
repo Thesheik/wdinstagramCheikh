f9b97790b73289b46efa8b2f5c4d0c1bdd9037f7
Cheikh Ndiongue <Cmndiongue@yahoo.com>
2015-07-28 08:29:20 -0400

    finished homework

---
+++ WDInstagram/app/controllers/entry_controller.rb
@@ -0,0 +1,39 @@
+class EntryController < ApplicationController
+  def index
+    @entries = Entry.all.order(:id)
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  # # create
+  # post "/entries" do
+  #   @entry = Entry.new( params[:entry] )
+  #   @entry.update( date_taken: Time.now.strftime("%B %d, %Y") )
+  #   if @entry.save
+  #     redirect_to ("/entries/#{@entry.id}")
+  #   else
+  #     redirect_to "/entries/new"
+  #   end
+  # end
+
+  def edit
+      @entry = Entry.find( params[:id] )
+  end
+
+  def update
+    @entry = Entry.find( params[:id] )
+    @entry.update(params[:entry] )
+    redirect_to ("/entries/#{@entry.id}")
+  end
+
+  def show
+    @entry = Entry.find( params[:id] )
+  end
+
+  def destroy
+    @entry = Entry.find( params[:id] )
+  end
+
+end
